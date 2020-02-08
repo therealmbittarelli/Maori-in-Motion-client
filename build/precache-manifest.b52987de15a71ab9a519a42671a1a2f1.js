5db3daddfa541eacb2eafd6a3084550fc2bc49fc
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-02-08 12:17:08 -0500

    first build

---
+++ build/precache-manifest.b52987de15a71ab9a519a42671a1a2f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4577e15807db6e2a0a57",
+    "url": "/static/js/main.4577e158.chunk.js"
+  },
+  {
+    "revision": "94894d8541f60a788251",
+    "url": "/static/js/2.94894d85.chunk.js"
+  },
+  {
+    "revision": "4577e15807db6e2a0a57",
+    "url": "/static/css/main.30856d6f.chunk.css"
+  },
+  {
+    "revision": "992467e7de64cf0cb92083dd8d3f184a",
+    "url": "/index.html"
+  }
+];
